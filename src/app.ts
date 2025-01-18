f3201b5d8bc024cb152be77830fbb9ca1dea5bc0
sapho-sys <saphonkunzi@gmail.com>
2025-01-18 08:32:15 +0200

    test connection

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ const startServer = () => {
   const app = createServer();
   const port: number = parseInt(<string>process.env.PORT, 10) || 4000;
   app.listen(port, () => {
-    console.log(`server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
   });
 };
 
